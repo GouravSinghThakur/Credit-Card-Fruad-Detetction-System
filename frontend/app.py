cddfa141f12ea998d8a8421f2751d3dc72640cda
Gourav_Singh_Thakur <144212296+GouravSinghThakur@users.noreply.github.com>
2026-06-19 10:59:48 +0530

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -1,41 +1,26 @@
 import streamlit as st
 import requests
 import numpy as np
-
+API_URL = "https://credit-card-fruad-detetction-system.onrender.com/predict"
 # Page configuration
 st.set_page_config(page_title="Fraud Detection", layout="wide")
-
 st.title("Credit Card Fraud Detection System")
-
 st.write("Enter transaction feature values to predict whether a transaction is fraudulent.")
-
 # Create input fields
 features = []
-
 for i in range(30):
-    value = st.number_input(f"Feature V{i+1}", value=0.0)
+    value = st.number_input(f"Feature V{i+1}", value=0)
     features.append(value)
-
 # Prediction button
 if st.button("Predict Transaction"):
-    API_URL = "https://credit-card-fruad-detetction-system.onrender.com/predict"
-
     try:
-        response = requests.post(
-            API_URL,
-            json={"features": features}
-        )
-
+        response = requests.post(API_URL,json={"features": features})
         result = response.json()
-
         st.subheader("Prediction Result")
-
         if result["prediction"] == "Fraud":
             st.error(f"Fraud Transaction ⚠️")
         else:
             st.success("Normal Transaction ✅")
-
         st.write(f"Fraud Probability: {result['probability']:.4f}")
-
     except:
         st.warning("API not running. Please start the FastAPI backend.")

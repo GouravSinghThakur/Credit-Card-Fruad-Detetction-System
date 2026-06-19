42a83a3c4d7dd9199dca8a3a88917c6fa332ec48
Gourav_Singh_Thakur <144212296+GouravSinghThakur@users.noreply.github.com>
2026-06-19 11:04:12 +0530

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -3,7 +3,7 @@ import requests
 import numpy as np
 API_URL = "https://credit-card-fruad-detetction-system.onrender.com/"
 # Page configuration
-st.set_page_config(page_title="Fraud Detection", layout="wide")
+st.set_page_config(page_title="Fraud Detection") #layout="wide")
 st.title("Credit Card Fraud Detection System")
 st.write("Enter transaction feature values to predict whether a transaction is fraudulent.")
 # Create input fields
